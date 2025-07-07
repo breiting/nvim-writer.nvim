8a6f33d03fc158e892e0f564cbcf9b718f8ced1a
Bernhard Reitinger <bernhard@reitinger.eu>
2025-07-07 21:56:00 +0200

    First version of writer exclusive mode

---
+++ writer.lua
@@ -0,0 +1,62 @@
+-- writer.lua
+
+-- Minimaler Start: keine Standard-Plugins, kein LSP
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.writebackup = false
+
+-- Keine Zeilennummern, keine Zeichenränder
+vim.opt.number = false
+vim.opt.relativenumber = false
+vim.opt.signcolumn = "no"
+vim.opt.statusline = ""
+vim.opt.laststatus = 2
+vim.opt.showmode = false
+vim.opt.cmdheight = 1
+vim.opt.ruler = false
+vim.opt.cursorline = false
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+vim.opt.colorcolumn = ""
+vim.opt.foldcolumn = "0"
+
+-- Schönes Schreiben
+vim.opt.linebreak = true
+vim.opt.wrap = true
+vim.opt.spell = true
+vim.opt.spelllang = { "de", "en" } -- Passe ggf. an
+vim.opt.textwidth = 80
+vim.opt.formatoptions:append("t")
+
+-- Optische Reduktion
+vim.opt.list = false
+vim.opt.fillchars:append({ eob = " " })
+
+-- Optisch angenehmes Theme
+vim.cmd("colorscheme default") -- Oder solarized/gruvbox etc., wenn du möchtest
+
+-- Wörterzähler für Statusline
+local function update_statusline()
+	local wc = vim.fn.wordcount()
+	local count = wc["words"] or 0
+	vim.opt.statusline = " Wörter: " .. count .. " "
+end
+
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		update_statusline()
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI", "BufEnter", "CursorHold" }, {
+	callback = function()
+		update_statusline()
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+	callback = function()
+		vim.keymap.set("n", "j", "gj", { buffer = true, silent = true })
+		vim.keymap.set("n", "k", "gk", { buffer = true, silent = true })
+	end,
+})
